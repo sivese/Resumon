6d8f6ce0ffef77dcca31a1ea8df102bcaefaf771
doss.lee <doss.lee@thundersoft.com>
2024-12-03 14:00:53 +0900

    canvas module

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,21 @@
 "use strict";
 const canvas = document.getElementById('mainCanvas');
+const context = canvas.getContext('2d');
+// TODO -> Auto resolution
+const WIDTH = 800;
+const HEIGHT = 800;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+function init() {
+    if (context == null)
+        throw new Error("Failed to get 2D context");
+    console.log("initializing .. ");
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+}
+try {
+    init();
+}
+catch (err) {
+    console.log(err);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 const canvas = document.getElementById('mainCanvas') as HTMLCanvasElement;
+const context = canvas.getContext('2d');
+
+
+// TODO -> Auto resolution
+const WIDTH  = 800;
+const HEIGHT = 800;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+function init() {
+    if(context == null) throw new Error("Failed to get 2D context");
+    
+    console.log("initializing .. ");
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+try {
+    init();
+} catch (err) {
+    console.log(err);
+}
